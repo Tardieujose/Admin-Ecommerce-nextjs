146e915acdfc2109bc92295de4710af281f0005a
Tardieujose <tardieujose@gmail.com>
2024-03-06 16:57:17 -0300

    Imagen BW

--- pages/index.js
+++ pages/index.js
@@ -207,13 +207,13 @@ export default function Home() {
       <div className="max-w-xl lg:max-w-3xl">
 
         <h1
-          className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
+          className="mt-6 text-2xl font-bold text-bgWolf sm:text-3xl md:text-4xl"
         >
-          Welcome to my-Shop
+          Bad Wolf Admin Panel
         </h1>
 
-        <p className="mt-4 leading-relaxed text-gray-500 max-w-sm">
-          This website is only accessible to admins only. Add new products and manage database.
+        <p className="mt-4 leading-relaxed text-colWolf max-w-sm">
+          This website is only accessible to admins only.
         </p>
         <div className="col-span-6 sm:flex sm:items-center sm:gap-4 my-4 flex items-center justify-center">
           <button
@@ -222,7 +222,7 @@ export default function Home() {
               setIsLoading(true);
               signIn('google');
             }}
-            className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+            className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-bold text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
           >
             Login With Google
           </button>
